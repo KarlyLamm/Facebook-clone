6606c9d171c965ce52e03385886f3a94d75a1fd0
Karly Lamm <klamm@voltsystems.net>
2022-02-20 20:27:15 -0600

    Added status component

--- src/Status.js
+++ src/Status.js
@@ -1,6 +1,39 @@
 import React from "react";
+import { FaFileImage, FaRegGrinAlt, FaVideo } from "react-icons/fa";
 
 const Status = () => {
-  return <div className="status"></div>;
+  return (
+    <div className="status">
+      <div className="statusf">
+        <div className="statusfimg">
+          <span>
+            <img src="../img/stockphoto1.jpeg" />
+          </span>
+        </div>
+        <div className="statusfinput">
+          <input
+            type="text"
+            className="statusfin"
+            placeholder="Put your thoughts here"
+          />
+        </div>
+      </div>
+      <div className="statusse">
+        <span className="statusicon">
+          <FaVideo className="live" />
+          <span className="text">Live</span>
+        </span>
+
+        <span className="statusicon">
+          <FaFileImage className="photo" />
+          <span className="text">Photos</span>
+        </span>
+        <span className="statusicon">
+          <FaRegGrinAlt className="feeling" />
+          <span className="text">Feeling</span>
+        </span>
+      </div>
+    </div>
+  );
 };
 export default Status;
